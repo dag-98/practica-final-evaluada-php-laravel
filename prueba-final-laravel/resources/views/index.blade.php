da40bff9c502259117006ed97b48b6bc08d57e68
dag-98 <daguilar.pacheco98@gmail.com>
2023-10-31 22:05:34 -0600

    se agrego boostrap

--- prueba-final-laravel/resources/views/index.blade.php
+++ prueba-final-laravel/resources/views/index.blade.php
@@ -1,75 +1,64 @@
-@extends('layouts.principal')
-
 @section('title', 'Proyectos')
 
 @section('content_header')
     <h1 style="text-align: center">Gestión de Proyectos</h1>
 @stop
 
-@section('content_body')
-    <div class="col-sm-12">
-        <a href="{{ route('units.create') }}"
-            class="btn btn-primary rounded-pill d-block d-sm-inline-block align-self-sm-center">
-            <i class="fas fa-user-friends"></i> Agregar Unidad
-        </a>
-    </div>
-    <br>
-    <br>
-    <div class="table-responsive">
-        <table class="table">
-                <thead class="table-dark" scope="col">#</thead>
-                <thead class="table-dark">Nombre Proyecto</thead>
-                <thead class="table-dark">Fuente Fondos </thead>
-                <thead class="table-dark">Monto Planificado</thead>
-                <thead class="table-dark">Monto Patrocinado</thead>
-                <thead class="table-dark">Monto Fondos Propios</thead>
-                
-                <th>Opciones</th>
-                <tbody>
-                    @foreach ($proyectos as $proyecto)
-                        <tr>
-                            <td>{{ $proyecto->id }}</td>
-                            <td>{{ $proyecto->NombreProyecto }}</td>
-                            <td>{{ $proyecto->fuenteFondos }}</td>
-                            <td>{{ $proyecto->MontoPlanificado }}</td>
-                            <td>{{ $proyecto->MontoPatrocinado }}</td>
-                            <td>{{ $proyecto->MontoFondosPropios }}</td>
-                            
-                            <td>
-                                <a href="{{ route('proyectos.show', $proyecto) }}">
-                                    <button class="btn btn-xs btn-outline-success mx-1 shadow" title="Editar">
-                                        <i class="far fa-lg fa-fw fa-eye"></i>
-                                    </button>
-                                </a>
-                                <a href="{{ route('proyectos.edit', $proyecto) }}">
-                                    <button class="btn btn-xs btn-outline-primary mx-1 shadow" title="Editar">
-                                        <i class="far fa-lg fa-fw fa-edit"></i>
-                                    </button>
-                                </a>
-                               
-                                <form action="{{ route('proyectos.destroy', $proyecto) }}" method="POST" style="display:inline;">
-                                    @method('DELETE')
-                                    @csrf
-                                    <a href="" style="color: inherit;">
-                                        <button type="submit" class="btn btn-xs btn-outline-danger mx-1 shadow" title="Eliminar"
-                                            onclick="return confirm('¿Estás seguro de que deseas eliminar este Proyecto?')">
-                                            <i class="far fa-lg fa-fw fa-trash-alt"></i>
-                                        </button>
-                                    </a>
-                                </form>
-                                
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-        </table>
-          
-    </div>
-    <br>
-    <br>
+@section('css')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 @stop
 
 
-@section('js')
+@section('content_body')
+    <div class="container mt-4">
+        <h1>Lista de Proyectos</h1>
 
+        <a href="{{ route('proyectos.create') }}" class="btn btn-primary">Crear Nuevo Proyecto</a>
+
+        <table class="table mt-4">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre del Proyecto</th>
+                    <th>Fuente de Fondos</th>
+                    <th>Monto Planificado</th>
+                    <th>Monto Patrocinado</th>
+                    <th>Monto Fondos Propios</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($proyectos as $proyecto)
+                    <tr>
+                        <td>{{ $proyecto->id }}</td>
+                        <td>{{ $proyecto->NombreProyecto }}</td>
+                        <td>{{ $proyecto->fuenteFondos }}</td>
+                        <td>${{ $proyecto->MontoPlanificado }}</td>
+                        <td>${{ $proyecto->MontoPatrocinado }}</td>
+                        <td>${{ $proyecto->MontoFondosPropios }}</td>
+                        <td>
+                            <a href="{{ route('proyectos.show', $proyecto->id) }}" class="btn btn-info"
+                                data-bs-toggle="modal" data-bs-target="#modalProyecto{{ $proyecto->id }}">Ver</a>
+                            <a href="{{ route('proyectos.edit', $proyecto->id) }}" class="btn btn-warning">Editar</a>
+                            <form action="{{ route('proyectos.destroy', $proyecto->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger"
+                                    onclick="return confirm('¿Estás seguro?')">Eliminar</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+@section('js')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 @stop
